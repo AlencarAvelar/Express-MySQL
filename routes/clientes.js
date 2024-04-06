bf952f2eeca6cc6b4fa9daed6e369fa090148638
AlencarAvelar <alencarhlavelar@gmail.com>
2024-04-06 02:23:34 -0300

    atualizando

--- routes/clientes.js
+++ routes/clientes.js
@@ -1,20 +1,26 @@
 const express = require('express');
 const router = express.Router();
+const clienteController =
+require('../controllers/clienteController');
 /* GET clientes*/
-router.get('/', function (req, res, next) {
- res.send('GET clientes');
-});
+router.get('/', clienteController.findAll);
 /* POST clientes*/
-router.post('/', function (req, res, next) {
- res.send('POST clientes');
-});
+router.post('/', clienteController.save);
 
 /* PUT clientes*/
-router.put('/', function (req, res, next) {
-    res.send('PUT clientes');
-   });
-   /* DELETE clientes*/
-   router.delete('/', function (req, res, next) {
-    res.send('DELETE clientes');
-   });
-   module.exports = router;
+router.put('/', clienteController.update);
+/* DELETE clientes*/
+router.delete('/:id', clienteController.remove);
+module.exports = router;
+
+const clienteController = require('../controllers/clienteController');
+const nomeMiddleware = require('../middlewares/nomeMiddleware');
+const sobrenomeMiddleware = require('../middlewares/sobrenomeMiddleware');
+const idadeMiddleware = require('../middlewares/idadeMiddleware');
+
+/* POST clientes*/
+router.post('/', nomeMiddleware.validateName,
+ sobrenomeMiddleware.validateFamilyName,
+ idadeMiddleware.validateAge,
+ clienteController.save
+);
