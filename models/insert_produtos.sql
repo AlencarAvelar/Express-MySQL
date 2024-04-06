56b096748e1c90d3522bb10d1f7d7540b98da4b4
AlencarAvelar <alencarhlavelar@gmail.com>
2024-04-06 02:13:26 -0300

    ajustes

---
+++ models/insert_produtos.sql
@@ -0,0 +1,6 @@
+INSERT INTO `produtos`(`Nome`, `Descrição`, `Preço`, `Data_Atualizada`) VALUES 
+('Smartphone', 'Um telefone celular inteligente com tela de alta resolução e câmera de alta qualidade.', 699, '2023-05-15'),
+('Notebook', 'Um computador portátil com processador potente e longa duração da bateria.', 1199, '2023-04-28'),
+('Fone de Ouvido com Cancelamento de Ruído', 'Fones de ouvido com tecnologia de cancelamento ativo de ruído para uma experiência auditiva imersiva.', 199, '2024-09-10'),
+('Smart TV', 'Uma televisão inteligente com resolução 4K e acesso a aplicativos de streaming.', 899, '2022-11-30'),
+('Console de Videogame', 'Um console de videogame de última geração com gráficos de alta definição e jogabilidade envolvente.', 399, '2021-08-20');
